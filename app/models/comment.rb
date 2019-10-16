318b71c23b57721925700e5c66cb81439a8e7a30
misigno <efournier92@gmail.com>
2019-10-16 19:59:36 -0400

    fix dependent destroy DB issue

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :census
+  belongs_to :user, dependent: :destroy
+  belongs_to :census, dependent: :destroy
 
   validates :comment, presence: :true
 end
